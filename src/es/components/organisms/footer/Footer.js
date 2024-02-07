47dbba943392c6d9dd876c4831216dc36ed5604b
edmgb <evangelos.dinopoulos@mgb.ch>
2024-02-07 11:30:53 +0100

    example update

--- src/es/components/organisms/footer/Footer.js
+++ src/es/components/organisms/footer/Footer.js
@@ -3,15 +3,15 @@
 import Footer from '../../../../es/components/web-components-toolbox/src/es/components/organisms/footer/Footer.js'
 
 export default class FooterCustom extends Footer {
-  renderCSS () {
-    super.renderCSS()
-    return this.fetchTemplate()
-  }
-
   fetchTemplate () {
     super.fetchTemplate()
 
     switch (this.getAttribute('namespace')) {
+      // case 'footer-custom-':
+      //   return this.fetchCSS([{
+      //     path: `${this.importMetaUrl}../../../../../../organisms/footer/custom-/custom-.css`, // apply namespace since it is specific and no fallback
+      //     namespace: false
+      //   }])
       case 'footer-custom-':
         return this.fetchCSS([{
           path: `${this.importMetaUrl}./default-/default-.css`,
