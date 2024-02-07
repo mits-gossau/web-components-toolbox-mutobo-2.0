1002b4695e4a03d18e2881bcc61607bfe2f41825
edmgb <evangelos.dinopoulos@mgb.ch>
2024-02-07 10:26:12 +0100

    update custom example

--- src/es/components/organisms/footer/Footer.js
+++ src/es/components/organisms/footer/Footer.js
@@ -3,30 +3,30 @@
 import Footer from '../../../../es/components/web-components-toolbox/src/es/components/organisms/footer/Footer.js'
 
 export default class FooterCustom extends Footer {
-  connectedCallback () {
-    this.renderExtCSS()
-  }
-
-  renderExtCSS () {
-    this.css = /* css */`
-      :host {
-        background-color:var(--invert-background-color, yellow); /* TESTING! */
-      }
-      
-      @media only screen and (max-width: _max-width_) {
-        :host {}
-      }
-    `
+  renderCSS () {
+    super.renderCSS()
     return this.fetchTemplate()
   }
 
   fetchTemplate () {
+    super.fetchTemplate()
+
     switch (this.getAttribute('namespace')) {
       case 'footer-custom-':
         return this.fetchCSS([{
-          path: '.../../../../organisms/footer/custom-/custom-.css',
+          path: `${this.importMetaUrl}./default-/default-.css`,
+          namespace: false,
+          replaces: [{
+            pattern: '--footer-default-',
+            flags: 'g',
+            replacement: '--footer-custom-'
+          }]
+        }, {
+          path: `${this.importMetaUrl}../../../../../../organisms/footer/custom-/custom-.css`,
           namespace: false
-        }])
+        }], false)
+      default:
+        return this.fetchCSS()
     }
   }
 }
